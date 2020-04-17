647c6c0645d34da6a35b167f8e03406fc0a1c07a
SidharthJ Dev <sid.j.dev@gmail.com>
2020-04-17 22:22:53 +0530

    Initial Commit

---
+++ Movies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Sidharth J Dev on 17/04/20.
+//  Copyright © 2020 Sidharth J Dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
